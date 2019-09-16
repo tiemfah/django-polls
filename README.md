65c217aa3dd0c97e215b33238e2c07cc8072c2bb
Sivanat Subpaisarn <sivanats@Sivanats-MacBook-Pro.local>
2019-09-16 15:09:31 +0700

    add external config file

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # Django-polls
 6110545643 Sivanat Subpaisarn
- 
 
-# About this project
-A tutorial project made using python with Django framework.
-In this case i was made polls app.
+## Django Polls Application
+(description.)
+A tutorial project made using Python(3.7) with Django(2.2) framework.
+In this case i made polls app.
+
+## Requirements
+* Python 3.6 or newer
+* Django 2.1.2 or newer
+* Python add-on modules as in [requirements.txt](requirements.txt)
+
+## Running
+* make sure you meet all requirement.
+* then type this ``` python3 manage.py runserver```
 
 ---
 
-###### last update 9 Aug 2019
+###### last update 16 Sep 2019
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!d*n6bn%5rh41yi=szjnw2g5rtqi-dn3a-t(bxis9$rin#@f=*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
