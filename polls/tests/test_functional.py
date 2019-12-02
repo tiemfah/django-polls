f6a92cd382a4a1d27f599bd5d72e8873e2602976
tiemfah <sivanat.sub@gmail.com>
2019-12-02 09:23:03 +0700

    add more test to selenium

--- polls/tests/test_functional.py
+++ polls/tests/test_functional.py
@@ -2,13 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
 
 
 class pollTest(LiveServerTestCase):
+    USER = 'testUser'
+    PASSWORD = 'testPassword'
+
     def setUp(self):
-        self.driver = webdriver.Chrome()
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
         # self.driver.add_argument('--headless')
         super(pollTest, self).setUp()
 
@@ -19,4 +23,21 @@ class pollTest(LiveServerTestCase):
     def test_poll(self):
         self.driver.get(self.live_server_url + '/polls/')
         h1 = self.driver.find_element_by_tag_name('h1')
-        self.assertIn('Please', h1.text)
+        self.assertIn('Current Polls', h1.text)
+
+    def test_not_authenticated(self):
+        self.driver.get(self.live_server_url)
+        h5 = self.driver.find_element_by_tag_name('h5')
+        self.assertIn('to vote for a topic', h5.text)
+
+    def test__authenticated(self):
+        User.objects.create_user(self.USER, password=self.PASSWORD)
+        self.driver.get(self.live_server_url + '/accounts/login')
+        self.driver.find_element_by_id(
+            "id_username").send_keys(self.USER)
+        self.driver.find_element_by_id(
+            "id_password").send_keys(self.PASSWORD)
+        self.driver.find_element_by_tag_name("button").click()
+        self.driver.get(self.live_server_url)
+        h5 = self.driver.find_element_by_tag_name('h5')
+        self.assertIn('Welcome back, ', h5.text)
